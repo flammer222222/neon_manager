1a3b4b61b8fda4f8cfda277fce80a7d534c2362a
Alamez <flammer222222.com>
2022-02-22 10:39:18 +0300

    Добавил установку докера

--- install/install_operator.sh
+++ install/install_operator.sh
@@ -61,6 +61,8 @@ install_operator () {
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
   # Linux post-install
   groupadd docker
   usermod -aG docker $USER
